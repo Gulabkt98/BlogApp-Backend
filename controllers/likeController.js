730be7f662f729e2affabfd36ad0700ef236caf4
Gulaba  Ram <gulabgkg99@gmail.com>
2025-07-30 12:06:12 +0530

    done

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,3 +1,35 @@
+ //import model
+ const Post= require("../models/postModel");
+ const Like =require("../models/likemodel");
+
+ //like a post
+   exports.likePost= async (req,res)=>{
+      try{
+         const{post,user}=req.body;
+         const like=new Like({
+            post,user,
+         });
+         const savedLike=await like.save();
+
+
+         //update the post collection basis on this 
+          const updatedPost= await Post.findByIdAndUpdate(post,{$push:{likes:savedLike._id}},{new:true});
+           res.json({
+            post:updatedPost,
+           })
+      }
+      catch(error){
+           console.error("Error in likePost:", error.message)
+           return res.status(400).json({
+            error:"Error while fetching post",
+           });
+
+      }
+   }
+ 
+ 
+ 
+ 
  exports.dummyLink=(req,res)=>{
     res.send("This is your  Dummy page");
- }
+ } 
--- routes/blog.js
+++ routes/blog.js
@@ -5,7 +5,7 @@ const router= express.Router();
 const {dummyLink}=require("../controllers/likeController");
 const{createComment} =require("../controllers/commentController");
 const{createPost,getAllPosts}=require("../controllers/postController");
-
+const{likePost}=require("../controllers/likeController");
 
 
 //mapping create 
@@ -13,6 +13,7 @@ router.get("/dummyroute",dummyLink);
 router.post("/comments/create",createComment);
 router.post("/posts/create",createPost);
 router.get("/posts",getAllPosts);
+router.post("/likes/like",likePost);
 
 //exports
 module.exports=router;
